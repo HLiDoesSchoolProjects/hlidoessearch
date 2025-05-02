4514469ae4f073bd7ef2e91589a35047d89dea24
HLiDoesSchoolProjects <167094680+HLiDoesSchoolProjects@users.noreply.github.com>
2025-05-02 08:09:22 -0500

    hlidoessearch

--- api.js
+++ api.js
@@ -10,9 +10,8 @@ const data = JSON.parse(fs.readFileSync("data/data.json", "utf-8"));
 app.use(express.static("web")); 
 
 app.get("/api/search", (req, res) => {
-    if (req.query.key !== "hlidoessearchsecretkey") return res.send(`{"error":"Disallowed"}`);
-    if (!req.query.s) return res.send(`{"error":"Please specify query string!"}`);
-    const queryWords = req.query.s.trim().toLowerCase().split(" ").map(word => stemmer(word));
+    if (!req.query.q) return res.send("Error: please specify query string!");
+    const queryWords = req.query.q.trim().toLowerCase().split(" ").map(word => stemmer(word));
 
     const infoData = data.map(item => {
         let score = 0;
@@ -28,13 +27,8 @@ app.get("/api/search", (req, res) => {
             score: score
         };
     });
-    const sortedInfoData = infoData.filter(item => item.score > 0).toSorted((a, b) => b.score - a.score);
-    const slicedResults = sortedInfoData.slice(0, req.query.count ?? 20);
-    return res.send({
-        count: slicedResults.length,
-        keywords: queryWords,
-        results: slicedResults
-    });
+    const slicedSortedInfoData = infoData.filter(item => item.score > 0).toSorted((a, b) => b.score - a.score).slice(0, req.query.count ?? 20);
+    return res.send(slicedSortedInfoData);
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
